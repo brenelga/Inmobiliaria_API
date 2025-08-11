b515daf54ff45906dfd9d87ab4c02955ef2ad972
Brenel Galicia <jesus.galicia.23e@utzmg.edu.mx>
2025-08-11 13:10:26 -0600

    delete server.php

--- server.php
+++
@@ -1,8 +0,0 @@
-<?php
-$port = (int)($_ENV['PORT'] ?? 8000);
-$command = sprintf(
-    'php -S 0.0.0.0:%d -t public %s',
-    $port,
-    escapeshellarg(__DIR__.'/public/index.php')
-);
-passthru($command);
